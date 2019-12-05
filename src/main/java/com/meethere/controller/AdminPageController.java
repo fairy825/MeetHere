85abae081df6a4224df04ad80fc34fc83093acb8
xiangyiwen <45915319+xiangyiwen@users.noreply.github.com>
2019-12-05 12:11:40 +0800

    first.html完善

--- src/main/java/com/meethere/controller/AdminPageController.java
+++ src/main/java/com/meethere/controller/AdminPageController.java
@@ -3,7 +3,6 @@ package com.meethere.controller;
 import com.meethere.pojo.Admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 

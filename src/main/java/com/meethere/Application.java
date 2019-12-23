42b8a43afefb8d30f32fbf0587fea73354452ac2
fairy825 <1324240571@qq.com>
2019-12-23 23:41:38 +0800

    增加了对手机号和邮箱的判断

--- src/main/java/com/meethere/Application.java
+++ src/main/java/com/meethere/Application.java
@@ -10,21 +10,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * jqgrid
- */
 
 @SpringBootApplication
 @EnableScheduling
-        //(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})//@EnableCaching
-//@EnableElasticsearchRepositories(basePackages = "com.how2java.tmall.es")
-//@EnableJpaRepositories(basePackages = {"com.how2java.tmall.dao", "com.how2java.tmall.pojo"})
 public class Application {
-//    static {
-//        PortUtil.checkPort(6379,"Redis 服务端",true);
-//        PortUtil.checkPort(9300,"ElasticSearch 服务端",true);
-//        PortUtil.checkPort(5601,"Kibana 工具", true);
-//    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
